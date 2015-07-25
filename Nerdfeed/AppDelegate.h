a983427830f99e02956785f5b8af7cb143ac7575
rebornwhu <rebornwhu@gmail.com>
2015-07-25 16:36:30 -0700

    First commit

---
+++ Nerdfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Xiao Lu on 7/25/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nerdfeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nerdfeed
+//
+//  Created by Xiao Lu on 7/25/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
