ac329001dced99f887eec46a5878d2574d3192fe
rebornwhu <rebornwhu@gmail.com>
2015-07-25 20:07:52 -0700

    Get data from web service and load them on TableView

--- Nerdfeed/BNRCoursesViewController.m
+++ Nerdfeed/BNRCoursesViewController.m
@@ -10,13 +10,33 @@
 
 @interface BNRCoursesViewController ()
 
+@property (nonatomic) NSURLSession *session;
+@property (nonatomic, copy) NSArray *courses;
+
 @end
+
 
 @implementation BNRCoursesViewController
 
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        self.navigationController.title = @"BNRCourses";
+        
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config
+                                                 delegate:nil
+                                            delegateQueue:nil];
+        [self fetchFeed];
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,12 +47,37 @@
 #pragma mark - UITableViewDelegate
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return [self.courses count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                                            forIndexPath:indexPath];
+    NSDictionary *course = self.courses[indexPath.row];
+    cell.textLabel.text = course[@"title"];
+    return cell;
+}
+
+#pragma mark - Custom methods
+- (void)fetchFeed
+{
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:req
+                                                     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                     NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data
+                                                                                                                options:0
+                                                                                                                  error:nil];
+                                                         self.courses = jsonObject[@"courses"];
+                                                         NSLog(@"%@", self.courses);
+                                                         dispatch_async(dispatch_get_main_queue(), ^{
+                                                             [self.tableView reloadData];
+                                                         });
+                                                     }];
+    [dataTask resume];
 }
 
 @end
